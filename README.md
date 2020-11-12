1e4059f60b2bf3fe116bfb8f25046b717c6d90cd
kis9a <kait0.min.w@gmail.com>
2020-11-12 16:51:48 +0900

    add exec mode test or production

--- README.md
+++ README.md
@@ -1,36 +1,9 @@
 # Gist2localmd
 
-### Completion
-
-```sh
-#!/bin/bash
-
-$(rm gists.json)
-$(rm -rf gists)
-$(mkdir gists)
-$(curl -u kis9a https://api.github.com/users/kis9a/gists >> gists.json)
-
-gists=$(cat gists.json)
-gists_length=$(jq length ./gists.json)
-
-for i in $( seq 0 $(($gists_length - 1))); do
-  gist=$(echo $gists | jq .[$i])
-  gist_files=$(echo $gist | jq ".files" -r)
-  gist_files_length=$(echo $gist_files | jq length)
-  gist_file_names=$(echo $gist_files | jq keys)
-
-  for n in $( seq 0 $(($gist_files_length -1))); do
-    gist_file=$(echo $gist_files | jq ."$(echo $gist_file_names | jq .[$n])" -r)
-    gist_file_name=$(echo $gist_file | jq ."filename" -r)
-    gist_raw_url=$(echo $gist_file | jq ."raw_url" -r)
-    $(curl $gist_raw_url --output ./gists/"$gist_file_name".md)
-  done
-done
-  ```
-
 ### Github api overview
 
 ✅ Checkout
+
 - https://api.github.com/
 - https://api.github.com/users/kis9a
 - https://api.github.com/users/kis9a/gists
@@ -41,5 +14,6 @@ done
 ### Parse Json with jq
 
 ✅ Checkout
+
 - https://stedolan.github.io/jq/
 - https://qiita.com/ryo0301/items/2ac9d11f355f1cf52ea5
---
+++ gist2localmd.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+exec_mode='test' # 'production' or 'test'
+mock_file='./gists.json'
+output_dir='./gists'
+
+## read gists
+if [ $exec_mode = 'test' ] ; then
+  if [ -f $mock_file ] ; then
+    gists=$(cat $mock_file)
+  else
+    echo 'not mock files'
+    gists=$(curl -u kis9a https://api.github.com/users/kis9a/gists -o $mock_file)
+    gists=$(cat $mock_file)
+  fi
+elif [ $exec_mode = 'production' ] ; then
+  gists=$(curl -u kis9a https://api.github.com/users/kis9a/gists)
+else
+  echo 'ERROR: set correct status'
+  exit 1
+fi
+
+echo $gists
+
+## set output directory
+if [ -d $output_dir ] ; then
+  $(rm $output_dir/*)
+else
+  $(mkdir $output_dir )
+fi
+
+gists_length=$(echo $gists | jq length)
+
+## gists export local markdown files
+for i in $( seq 0 $(($gists_length - 1))); do
+  gist=$(echo $gists | jq .[$i])
+  gist_files=$(echo $gist | jq ".files" -r)
+  gist_files_length=$(echo $gist_files | jq length)
+  gist_file_names=$(echo $gist_files | jq keys)
+
+  echo $gist_files_length
+
+  for n in $( seq 0 $(($gist_files_length -1))); do
+    gist_file=$(echo $gist_files | jq ."$(echo $gist_file_names | jq .[$n])" -r)
+    gist_file_name=$(echo $gist_file | jq ."filename" -r)
+    gist_raw_url=$(echo $gist_file | jq ."raw_url" -r)
+    $(curl $gist_raw_url -o $output_dir/"$gist_file_name".md)
+  done
+done

c3bad6cabb53e6592b90eabcd482048d772c94d6
kis9a <kait0.min.w@gmail.com>
2020-12-05 22:09:04 +0900

    modify: gist2localmd.sh validators type definition

--- gist2localmd.sh
+++ gist2localmd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-exec_mode='test' # 'production' or 'test'
+exec_mode='production' # 'production' or 'test'
 mock_file='./gists.json'
 output_dir='./gists'
 
@@ -13,14 +13,14 @@ fi
 ## read gists or mock_file
 if [ $exec_mode = 'test' ] ; then
   if [ -f $mock_file ] ; then
-    gists=$(cat $mock_file)
+    declare -a gists=$(cat $mock_file)
   else
     echo 'dont exit mock files, get gists and output mock_file'
     $(curl -u $1 https://api.github.com/users/$2/gists -o $mock_file)
-    gists=$(cat $mock_file)
+    declare -a gists=$(cat $mock_file)
   fi
 else
-  gists=$(curl -u $1 https://api.github.com/users/$2/gists)
+  declare -a gists=$(curl -u $1 https://api.github.com/users/$2/gists)
 fi
 
 ## set output directory
@@ -28,20 +28,22 @@ $(rm -rf $output_dir) && $(mkdir $output_dir)
 
 ## set get gist_files times set. if exec_mode='test' will get 2 times get
 if [ $exec_mode = 'test' ]  ; then
-  gists_length=2
+  declare -i gists_length=2
 else
-  gists_length=$(echo $gists | jq length)
+  declare -i gists_length=$(echo $gists | jq length)
 fi
-
 
 ## gists export local markdown files
 for i in $( seq 0 $(($gists_length - 1))); do
   gist=$(echo $gists | jq .[$i])
   gist_files=$(echo $gist | jq ".files" -r)
-  gist_files_length=$(echo $gist_files | jq length)
+  declare -i gist_files_length=$(echo $gist_files | jq length)
   gist_file_names=$(echo $gist_files | jq keys)
 
-  echo $gist_files_length
+  if [ $gist_files_length = 0 ] ; then
+    echo 'dont exit gist_files in this gist'
+    exit;
+  fi
 
   for n in $( seq 0 $(($gist_files_length -1))); do
     gist_file=$(echo $gist_files | jq ."$(echo $gist_file_names | jq .[$n])" -r)
